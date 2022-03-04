983221c66a0f0e1d8c2a7e4fcff3209a1a141413
Flávio Bianchetti <fjb.developer@gmail.com>
2022-03-04 20:12:54 -0300

    Feat: DefaultValues.js - file with default TailwindCSS values

---
+++ src/data/DefaultValues.js
@@ -0,0 +1,10 @@
+const DefaultValues = {
+  defaultBGColor: 'bg-violet-400',
+  defaultBGColorHover: 'bg-violet-500',
+  defaultTextColor: 'text-violet-600',
+  defaultTextBGColor: 'text-white',
+  defaultBorderFocusColor: 'border-violet-500',
+  defaultBGWhite: 'bg-white',
+};
+
+export default DefaultValues;
